154d0b1c750b3bb5d18f062ef853e97ffdc6ded6
jp3pe <45834750+jp3pe@users.noreply.github.com>
2024-02-05 17:05:31 +0900

    버블 정렬 에니매이션 추가 (#1)
    * feature: 버블 정렬 전과 후를 canvas로 출력
    
    * feature: 그래프 시작점을 아래로 지정
    
    * refactor: 그래프 직각좌표계로 변경 기능을 canvas 객체 메소드로 대체
    
    * fix: 정렬에 버블 정렬 알고리즘을 사용하도록 수정
    
    기존 코드는 버블 정렬이라고 착각했던 코드를 사용 중이었음
    
    * add: 버블 정렬 애니미이션과 정적 대조군 보여주는 기능 추가
    
    * feature: 그래프 에니매이션에 색상 강조 기능 추가
    
    * feature: 비동기 처리가 되어 있지 않는 애니매이션 기능 추가
    
    * feature: 그래프 정렬 애니메이션 기능 추가
    
    * ref: 캔버스 translate 사용
    
    * up: 버블 정렬에서 중복되는 핵심 로직 함수화
    
    * feature: 데카르트 좌표계로 변경
    
    좌측 하단이 0,0 지점으로 설정됨
    
    * up: 애니메이션 제외한 나머지 canvas 삭제
    
    * del: 미사용 파일 삭제

--- main.js
+++ main.js
@@ -1,32 +1,130 @@
+const canvasWidth = 600;
+const canvasHeight = 300;
+const wait = (timeToDelay) =>
+  new Promise((resolve) => setTimeout(resolve, timeToDelay));
+
+function bubbleSortKeyFeature(arrayInput, index) {
+  let array = [...arrayInput];
+  let temp = 0;
+  temp = array[index + 1];
+  array[index + 1] = array[index];
+  array[index] = temp;
+
+  return array;
+}
+
 function bubbleSort(arrayInput) {
   let array = [...arrayInput];
-  const arrayLength = array.length;
+  let endIndex = array.length - 1;
 
-  let temp = 0;
-  for (let i = 0; i < arrayLength - 1; i++) {
-    for (let j = i + 1; j < arrayLength; j++) {
-      if (array[i] < array[j]) {
-        temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < endIndex; j++) {
+      if (array[j] > array[j + 1]) {
+        const tempArray = [...array];
+        array = bubbleSortKeyFeature(tempArray, j);
       }
     }
+    endIndex = endIndex - 1;
   }
 
   return array;
 }
 
-function printComparedArraies(array1, array2) {
-  console.log(`array1: ${array1}`);
-  console.log(`array2: ${array2}`);
+// Draw array instantly.
+function drawArrayOnCanvas(canvasId, array) {
+  const canvas = document.querySelector(`#${canvasId}`);
+  let canvasXPoint = 0;
+
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+    const graphWidth = canvasWidth / array.length;
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    for (const [index, item] of array.entries()) {
+      const graphHeight = (canvasHeight * item) / Math.max(...array);
+      ctx.fillRect(canvasXPoint, 0, graphWidth, graphHeight);
+      canvasXPoint = canvasXPoint + graphWidth;
+    }
+  }
 }
 
-const arrayLength = 20;
-const sampleArray = new Array(20);
-for (i = 0; i < arrayLength; i++) {
-  sampleArray[i] = Math.round(Math.random() * 10);
+async function drawBubbleSortAnimation(canvasId, arrayInput) {
+  let array = [...arrayInput];
+  let endIndex = array.length - 1;
+
+  const canvas = document.querySelector(`#${canvasId}`);
+  const graphWidth = canvasWidth / array.length;
+
+  // Start to draw animation on canvas
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+    // Set graph to cartesianCoordinateSystem
+    ctx.translate(0, canvasHeight);
+    ctx.scale(1, -1);
+
+    for (let i = 0; i < array.length - 1; i++) {
+      for (let j = 0; j < endIndex; j++) {
+        if (array[j] > array[j + 1]) {
+          const tallerGraphHeight =
+            (canvasHeight * array[j]) / Math.max(...array);
+          const shorterGraphHeight =
+            (canvasHeight * array[j + 1]) / Math.max(...array);
+          const canvasXPointStart = graphWidth * j;
+          let tallerXPoint = canvasXPointStart;
+          let shorterXPoint = canvasXPointStart + graphWidth;
+          ctx.clearRect(canvasXPointStart, 0, graphWidth * 2, canvasHeight);
+
+          // Draw graph before animating.
+          ctx.restore();
+          drawArrayOnCanvas(canvasId, array);
+
+          //Draw changing animation.
+          const animationMS = 2000;
+          ctx.save();
+          while (shorterXPoint >= canvasXPointStart) {
+            // Draw taller graph item.
+            ctx.fillStyle = "blue";
+            ctx.translate(tallerXPoint, 0);
+            ctx.fillRect(0, 0, graphWidth, tallerGraphHeight);
+            ctx.restore();
+            ctx.save();
+            // Move x grid to left.
+            tallerXPoint = tallerXPoint + 1;
+
+            // Draw shorter graph item.
+            ctx.fillStyle = "red";
+            ctx.translate(shorterXPoint, 0);
+            ctx.fillRect(0, 0, graphWidth, shorterGraphHeight);
+            ctx.restore();
+            ctx.save();
+            // Moving x grid to right.
+            shorterXPoint = shorterXPoint - 1;
+
+            await wait(5);
+            ctx.clearRect(canvasXPointStart, 0, graphWidth * 2, canvasHeight);
+          }
+
+          // Do real sorting.
+          const tempArray = [...array];
+          array = bubbleSortKeyFeature(tempArray, j);
+        }
+      }
+      endIndex = endIndex - 1;
+    }
+
+    drawArrayOnCanvas(canvasId, array);
+  }
 }
 
-const sortedArray = bubbleSort(sampleArray);
+function drawGraph() {
+  const arrayLength = 10;
+  const sampleArray = new Array(arrayLength);
+  for (i = 0; i < arrayLength; i++) {
+    sampleArray[i] = Math.round(Math.random() * 10);
+  }
+  const sortedArray = bubbleSort(sampleArray);
+  
+  drawBubbleSortAnimation("sortingAnimation", sampleArray);
+}
 
-printComparedArraies(sampleArray, sortedArray);
+drawGraph();
